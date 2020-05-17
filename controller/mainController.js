e00c504546739417cb0ee2f0814d0955813dcdc9
ckpd84 <ckpd84@gmail.com>
2020-05-17 20:20:03 -0300

    lectura de archivo json

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,6 +1,11 @@
+let fs = require("fs");
+let productosJSON = fs.readFileSync("products.json", { encoding: "utf-8" });
+let productos = JSON.parse(productosJSON);
+console.log(productos);
+
 let mainController = {
   index: (req, res) => {
-    res.render("index");
+    res.render("index", { productos: productos });
   },
 };
 
